4d74178174a72c1740debb76f532bad7582323b4
dpossamai <deividepossamai@gmail.com>
2016-05-22 17:17:21 -0300

    Separate front end code from back end

--- src/main/java/com/sec21/DemoApplication.java
+++ src/main/java/com/sec21/DemoApplication.java
@@ -1,12 +1,25 @@
 package com.sec21;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
-public class DemoApplication {
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class DemoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static Class<DemoApplication> applicationClass = DemoApplication.class;
 }
