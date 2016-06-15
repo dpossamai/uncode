e159249b93bcef073c5399f8263fa8456494b6bc
dpossamai <deividepossamai@gmail.com>
2016-06-14 21:01:05 -0300

    configured authentication URL.

--- src/main/java/com/sec21/config/SecurityConfiguration.java
+++ src/main/java/com/sec21/config/SecurityConfiguration.java
@@ -1,8 +1,10 @@
 package com.sec21.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CsrfFilter;
@@ -17,15 +19,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-//		 http.httpBasic()
-//		 .and().authorizeRequests()
-//		 .antMatchers("/","/home.html","/index.html", "/login.html","/user").permitAll().anyRequest().anonymous()
-//		 .and()
-//		 .authorizeRequests()
-//		 .antMatchers("/hue.html").permitAll().anyRequest().authenticated().and()
-//		 .addFilterAfter(new CsrfHeaderFilter(),	
-//		 CsrfFilter.class).csrf().csrfTokenRepository(csrfTokenRepository()).and()
-//		 .logout();
+		 http.httpBasic()
+		 .and().authorizeRequests()
+		 .antMatchers("/","/home.html","/index.html", "/login.html","/user").permitAll().anyRequest().anonymous()
+		 .and()
+		 .authorizeRequests()
+		 .antMatchers("/profile.html").permitAll().anyRequest().authenticated().and()
+		 .addFilterAfter(new CsrfHeaderFilter(),	
+		 CsrfFilter.class).csrf().csrfTokenRepository(csrfTokenRepository()).and()
+		 .logout();
+	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		  auth.inMemoryAuthentication().withUser("user").password("123").roles("USER");
 	}
 
 	private CsrfTokenRepository csrfTokenRepository() {
--- src/main/java/com/sec21/controllers/HomeController.java
+++ src/main/java/com/sec21/controllers/HomeController.java
@@ -5,15 +5,33 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 	
 	@RequestMapping("/user")
-	public Principal user(Principal user) {
-		return user;
+	public ResponseEntity<?> user(Principal user) {
+		return user == null ? new ResponseEntity<String>(HttpStatus.UNAUTHORIZED) : new ResponseEntity<Principal>(user, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public ResponseEntity<?> logoutPage (HttpServletRequest request, HttpServletResponse response) {
+	    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    if (auth != null){    
+	        new SecurityContextLogoutHandler().logout(request, response, auth);
+	    }
+	    return new ResponseEntity<String>(HttpStatus.OK);
 	}
 
 	@RequestMapping("/resource")
